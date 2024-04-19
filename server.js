a56af906003d7bc29b40c261f9c39e240a78f149
Abdul Gaffar <csabdulgaffar@gmail.com>
2024-04-19 17:10:37 +0530

    'changes;

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors());
 // Set up middleware and routes
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'contact.html'));
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 
 })
 
